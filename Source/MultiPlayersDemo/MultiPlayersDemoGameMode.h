b938708889478716f309e034e457fb7de6abdbee
syane <syane1@163.com>
2020-01-13 06:40:03 +0800

    Initial Commit

---
+++ Source/MultiPlayersDemo/MultiPlayersDemoGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MultiPlayersDemoGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AMultiPlayersDemoGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AMultiPlayersDemoGameMode();
+};
+
+
+
